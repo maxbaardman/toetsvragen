2c8fb605d53ec74f85fcb26cfb7f370acb605da4
maxbaardman <70890694+maxbaardman@users.noreply.github.com>
2022-05-16 14:21:23 +0200

    Add files via upload

---
+++ opdracht3.php
@@ -0,0 +1,14 @@
+<?php 
+print("maak een array van deze variabelen <br>");
+
+
+$array = 1;
+$array = 2;
+$array = 3;
+
+print_r($array);
+
+
+?>
+
+<!-- antwoord je moet [] achter de variablen zeten waarom [] erachter omdat als je er [] achter zet kan je er meerdere values in opslaan -->
